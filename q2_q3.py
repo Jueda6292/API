51ca64b8eda870dbdfeba7faa4a2ccbe88bb9bbb
Jueda6292 <85045566+Jueda6292@users.noreply.github.com>
2021-10-11 23:57:02 -0400

    Update and rename jueda.py to q2_q3.py

--- q2_q3.py
+++ q2_q3.py
@@ -1,12 +1,7 @@
 import geocoder
 import requests
 
-# Replace XXXXXXXXXXXX with your actual key, which
-# will look like a bunch of letters and numbers!
 API_KEY = 'XXXXXXXXXXXXXXXXXXXX'
-# When you are done with the homework, remove your actual key
-# and replace it back with XXXXXXXXXXXXXXXXXX before you commit
-# and push to Github.
 
 API_BASE_URL = f'httpsapi.openweathermap.orgdata2.5weatherunits=imperial&appid={API_KEY}'
 
@@ -25,17 +20,9 @@ def main()
     'Capilano Suspension Bridge',
   ]
 
-  # Loop through each destination
   for destination in destinations
     g = geocoder.arcgis(destination)
     print (f'The {destination} is located at ({g.latlng[0].2f}, {g.latlng[1].2f})')
-    ###########
-    # COPY & PASTE RELEVANT CODE FROM PART 1 HERE
-    ###########
-
-    # You'll need to construct the full API url with the latitude and longitude,
-    # with something like this
-    # full_api_url = API_BASE_URL + '&lat=' + latitude + '&lon=' + longitude
 
     lat = f'{g.latlng[0].2f}'
     lng = f'{g.latlng[1].2f}'
@@ -46,10 +33,6 @@ def main()
     result = requests.get(full_api_url).json()
 
     print(fAt the {destination} right now, it's {result['weather'][0]['description']} with a temperature of {result['main']['temp'].1f}u00B0Fn)
-    # result = requests.get(full_api_url).json()
-
-
-    # From the result, print ousummaryt the  and current temperature
 
 
 main()
